881fe3c0923135c0a1032d4f0f6acbbe364618a3
zxhamo <38841829+zxhamo@users.noreply.github.com>
2018-05-31 08:17:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,48 +11,44 @@ client.on('message', message => {
       }
 });
 
+clien.on('message', message => {
+      if(message.content == '!قران 1'){
+        message.react('🔊')}  return;
+  const ytdl = require('ytdl-core');
+    const stram0ptions = { seek: 0, volume: 100};  
+    const broadcast = 
+clien.createVoiceBroadcast();
 
-client.on('message', message => {
-  if(!message.channel.guild) return;
-if(message.content.startsWith('$bc')) {
-if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-let copy = "Dragon";
-let request = `Requested By ${message.author.username}`;
-if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
-msg.react('✅')
-.then(() => msg.react('❌'))
-.then(() =>msg.react('✅'))
 
-let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
-reaction1.on("collect", r => {
-message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
-message.guild.members.forEach(m => {
-var bc = new
-Discord.RichEmbed()
-.setColor('RANDOM')
-.setTitle('Broadcast')
-.addField('Server', message.guild.name)
-.addField('Sender', message.author.username)
-.addField('Message', args)
-.setThumbnail(message.author.avatarURL)
-.setFooter(copy, client.user.avatarURL);
-m.send({ embed: bc })
-msg.delete();
-})
-})
-reaction2.on("collect", r => {
-message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
-msg.delete();
-})
-})
-}
+
+message.gulid.member(message.author).voiceChannel.join()
+       .then(connection => {
+           const stream = ytdl('https://www.youtube.com/watch?v=Ktync4j_nmA', { filter : 'audioonly'});
+           broadcast.playstream(stream);
+           const dispatcher = connection.playBroadcast(broadcast);
+ })
+.catch(console.error);
 });
 
+clien.on('messge', message => {
+       if(message.content == '!قران 2'){
+           message.react('🔊')} return;
+    const ytdl = require('ytdl-core');
+      const stram0ptions = { seek: 0, volume:100};
+      const broadcast = 
+clien.createVoiceBroadcast();
+
+
+
+
+message.gulid.member(message.author).voiceChannel.join()
+       .then(connection => {
+           const stream = ytdl('https://www.youtube.com/watch?v=E1vpbnkTmUE' ,{ filter : 'audioonly' });
+           broadcast.playstream(stream);
+           const dispatcher = connection.playBroadcast(broadcast);
+       })
+.catch(console.error);
+})
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
