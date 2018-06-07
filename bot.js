3d1bc39f7fd1923b1952b826c86066e2eb985b5e
zxhamo <38841829+zxhamo@users.noreply.github.com>
2018-06-07 19:55:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,44 +11,13 @@ client.on('message', message => {
       }
 });
 
-clien.on('message', message => {
-      if(message.content == '!قران 1'){
-        message.react('🔊')}  return;
-  const ytdl = require('ytdl-core');
-    const stram0ptions = { seek: 0, volume: 100};  
-    const broadcast = 
-clien.createVoiceBroadcast();
 
+client.on('message', message => {
+ if (message.content === x_x) {
+  client.user.setGame(`LINKS | i love PL`, 'https://www.twitch.tv/hix')
 
-
-message.gulid.member(message.author).voiceChannel.join()
-       .then(connection => {
-           const stream = ytdl('https://www.youtube.com/watch?v=Ktync4j_nmA', { filter : 'audioonly'});
-           broadcast.playstream(stream);
-           const dispatcher = connection.playBroadcast(broadcast);
- })
-.catch(console.error);
+ }
 });
-
-clien.on('messge', message => {
-       if(message.content == '!قران 2'){
-           message.react('🔊')} return;
-    const ytdl = require('ytdl-core');
-      const stram0ptions = { seek: 0, volume:100};
-      const broadcast = 
-clien.createVoiceBroadcast();
-
-
-
-
-message.gulid.member(message.author).voiceChannel.join()
-       .then(connection => {
-           const stream = ytdl('https://www.youtube.com/watch?v=E1vpbnkTmUE' ,{ filter : 'audioonly' });
-           broadcast.playstream(stream);
-           const dispatcher = connection.playBroadcast(broadcast);
-       })
-.catch(console.error);
-})
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
